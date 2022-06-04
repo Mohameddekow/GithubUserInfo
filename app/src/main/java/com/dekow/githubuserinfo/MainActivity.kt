4c8a9304bce1d92c5831750b4d7f08d686bcfefe
mohamed <mohameddekow77@gmail.com>
2022-06-04 10:32:51 +0300

    Initial commit

---
+++ app/src/main/java/com/dekow/githubuserinfo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.dekow.githubuserinfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dekow.githubuserinfo.ui.theme.GithubUserInfoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GithubUserInfoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GithubUserInfoTheme {
+
+    }
+}
